5f374af8f010d8ce93beee17708b930ae53de5c6
Guohao <13524286564@163.com>
2017-04-28 12:01:09 +0800

    riso-wechat

---
+++ riso-wechat/src/wechat/components/my.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my'
+}
+</script>
+
+<style lang="stylus">
+  .container
+    height 100%
+</style>
